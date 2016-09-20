d723e06a6651e36b8257a80d192bbc6fa3ab1439
Kashif Iftikhar <kashif@compulife.com.pk>
2016-09-21 03:06:07 +0500

    Use bind vars for AQL query construction

--- arango_orm/query.py
+++ arango_orm/query.py
@@ -16,6 +16,7 @@ class Query(object):
 
         self._db = db
         self._CollectionClass = CollectionClass
+        self._bind_vars = {'@collection': self._CollectionClass.__collection__}
 
     def count(self):
         "Return collection count"
@@ -25,10 +26,9 @@ class Query(object):
     def all(self):
         "Return all records considering current filter conditions (if any)"
 
-        aql = 'FOR rec IN {collection} RETURN rec'.format(
-            collection=self._CollectionClass.__collection__)
+        aql = 'FOR rec IN @@collection RETURN rec'
 
-        results = self._db.aql.execute(aql)
+        results = self._db.aql.execute(aql, bind_vars=self._bind_vars)
         ret = []
 
         for rec in results:
