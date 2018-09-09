64a2b11ec226852ca028360fb81a682837f68562
Kashif Iftikhar <kashif@compulife.com.pk>
2018-09-09 16:29:38 +0500

    v4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 requires = [
     'six',
-    'python-arango',
+    'python-arango>=4.0',
     'marshmallow~=2.10.0'
 ]
 
@@ -10,14 +10,15 @@ setup(
     name='arango-orm',
     version='0.4',
     description='A SQLAlchemy like ORM implementation for arangodb',
-    long_description="A SQLAlchemy like ORM implementation using python-arango as the backend library",
+    long_description=("A SQLAlchemy like ORM implementation using "
+                      " python-arangoas the backend library"),
     classifiers=[
         "Programming Language :: Python"
     ],
     author='Kashif Iftikhar',
     author_email='kashif@compulife.com.pk',
     url="https://github.com/threatify/arango-orm",
-    download_url="https://github.com/threatify/arango-orm/archive/0.3.tar.gz",
+    download_url="https://github.com/threatify/arango-orm/archive/0.4.tar.gz",
     license="GNU General Public License v3 (GPLv3)",
     keywords='arangodb orm python',
     packages=find_packages(),
